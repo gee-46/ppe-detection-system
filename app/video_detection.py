460464d2633b5e32f8c6e39983e38b7779c1d2ea
Gautam N Chipkar <gautamchipkar46@gmail.com>
2026-06-05 23:58:12 +0530

    Update video_detection.py

--- app/video_detection.py
+++ app/video_detection.py
@@ -41,7 +41,6 @@ while cap.isOpened():
     cv2.imshow("Video Detection", annotated_frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-
+        
 cap.release()
 cv2.destroyAllWindows()
