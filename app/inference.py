b6ea769ef55fca89dca2d2559b30f81fe946c662
Gautam N Chipkar <gautamchipkar46@gmail.com>
2026-06-13 23:34:38 +0530

    Update inference.py

--- app/inference.py
+++ app/inference.py
@@ -1,1 +1,13 @@
+from ultralytics import YOLO
+
+# Load pretrained YOLOv8 model
+model = YOLO("yolov8n.pt")
+
+# Run object detection
+results = model(
+    "https://ultralytics.com/images/bus.jpg",
+    save=True
+)
+
+print("Detection completed successfully!")
 
