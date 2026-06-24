28d149d035ff0575e5cee2a8067dc6a7bba2d15d
Gautam N Chipkar <gautamchipkar46@gmail.com>
2026-06-24 23:50:56 +0530

    Update detection_analysis.py

--- app/detection_analysis.py
+++ app/detection_analysis.py
@@ -1,1 +1,26 @@
+from ultralytics import YOLO
+
+# Load model
+model = YOLO("yolov8n.pt")
+
+# Run detection
+results = model("bus.jpg")
+
+for result in results:
+
+    boxes = result.boxes
+
+    for box in boxes:
+
+        class_id = int(box.cls[0])
+
+        confidence = float(box.conf[0])
+
+        class_name = model.names[class_id]
+
+        
+        print(
+            f"Object: {class_name} | "
+            f"Confidence: {confidence:.2f}"
+        )
 
